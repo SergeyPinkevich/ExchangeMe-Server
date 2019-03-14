24ee7cfbac2b86fa257faddbd477349d1ae0c3df
Sergey Pinkevich <s.pinkevich@cian.ru>
2019-03-14 21:08:56 +0300

    countries list

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+import json
+
 import requests
+from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +10,12 @@ app = Flask(__name__)
 @app.route('/rates')
 def hello_world():
     return requests.get("http://www.floatrates.com/daily/usd.json").text
+
+
+@app.route('/countries')
+def countries():
+    with open('static/countries.json') as data:
+        return jsonify(json.load(data))
 
 
 if __name__ == '__main__':
